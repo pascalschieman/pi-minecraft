f96cedaed9cec6a664a23984626be4d45e002d25
Bandiet <pascal.schieman@gmail.com>
2025-01-07 21:21:32 +0100

    initial version tested in linux dev container

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo apt install wget -y
+#wget -O zulu-jdk.deb 'https://cdn.azul.com/zulu/bin/zulu23.30.13-ca-jdk23.0.1-linux_arm64.deb?_gl=1*9354sk*_gcl_au*MTQxMTg2Mzg5Mi4xNzM2Mjc3Nzg0*_ga*NjM5NTk4NDU5LjE3MzYyNzc3ODQ.*_ga_42DEGWGYD5*MTczNjI3Nzc4NC4xLjEuMTczNjI3ODEyMC41OS4wLjA.'
+#sudo dpkg -i zulu-jdk.deb
+#sudo apt --fix-broken install -y  # Fehler beheben und fehlende Abhängigkeiten installieren
+#rm zulu-jdk.deb  # .deb-Datei löschen, um Platz zu sparen
+
+mkdir papermcserver
+cd papermcserver
+wget https://api.papermc.io/v2/projects/paper/versions/1.21.4/builds/77/downloads/paper-1.21.4-77.jar
+mv paper-1.21.4-77.jar papermcserver.jar
+mkdir plugins
+cd plugins
+wget -O Geyser-Spigot.jar https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot
+#mv spigot Geyser-Spigot.jar
+wget -O floodgate-spigot.jar https://download.geysermc.org/v2/projects/floodgate/versions/latest/builds/latest/downloads/spigot
+#mv spigot floodgate-spigot.jar
+cd ..
+
+#first server initialization
+java -Xms2G -Xmx4G -jar papermcserver.jar nogui &
+SERVER_PID=$!
+sleep 20
+echo "eula=true" > eula.txt
+
+java -Xms2G -Xmx4G -jar papermcserver.jar nogui &
+
+#cd plugins/Geyser-Spigot
+#sed -i 's/auth-type: .*/auth-type: floodgate/' config.yml
+#screen -dmS papermcserver java -Xms1G -Xmx2G -jar papermcserver.jar nogui
