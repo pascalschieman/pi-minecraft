131533f72c7da482f28230910c2674e2841361dc
Bandiet <pascal.schieman@gmail.com>
2025-01-09 13:13:58 +0100

    changes for raspberry pi environment

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo apt install wget -y
-#wget -O zulu-jdk.deb 'https://cdn.azul.com/zulu/bin/zulu23.30.13-ca-jdk23.0.1-linux_arm64.deb?_gl=1*9354sk*_gcl_au*MTQxMTg2Mzg5Mi4xNzM2Mjc3Nzg0*_ga*NjM5NTk4NDU5LjE3MzYyNzc3ODQ.*_ga_42DEGWGYD5*MTczNjI3Nzc4NC4xLjEuMTczNjI3ODEyMC41OS4wLjA.'
-#sudo dpkg -i zulu-jdk.deb
-#sudo apt --fix-broken install -y  # Fehler beheben und fehlende Abhängigkeiten installieren
-#rm zulu-jdk.deb  # .deb-Datei löschen, um Platz zu sparen
+wget -O zulu-jdk.deb 'https://cdn.azul.com/zulu/bin/zulu23.30.13-ca-jdk23.0.1-linux_arm64.deb?_gl=1*9354sk*_gcl_au*MTQxMTg2Mzg5Mi4xNzM2Mjc3Nzg0*_ga*NjM5NTk4NDU5LjE3MzYyNzc3ODQ.*_ga_42DEGWGYD5*MTczNjI3Nzc4NC4xLjEuMTczNjI3ODEyMC41OS4wLjA.'
+sudo dpkg -i zulu-jdk.deb
+sudo apt --fix-broken install -y  # Fehler beheben und fehlende Abhängigkeiten installieren
+rm zulu-jdk.deb  # .deb-Datei löschen, um Platz zu sparen
 
 mkdir papermcserver
 cd papermcserver
@@ -26,6 +26,6 @@ echo "eula=true" > eula.txt
 
 java -Xms2G -Xmx4G -jar papermcserver.jar nogui &
 
-#cd plugins/Geyser-Spigot
-#sed -i 's/auth-type: .*/auth-type: floodgate/' config.yml
-#screen -dmS papermcserver java -Xms1G -Xmx2G -jar papermcserver.jar nogui
+cd plugins/Geyser-Spigot
+sed -i 's/auth-type: .*/auth-type: floodgate/' config.yml
+screen -dmS papermcserver java -Xms1G -Xmx2G -jar papermcserver.jar nogui
