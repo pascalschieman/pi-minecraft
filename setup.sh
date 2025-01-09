cc310d2475af7e4468b16503678642aa8e27c7ad
Bandiet <pascal.schieman@gmail.com>
2025-01-09 17:41:23 +0100

    killing process causes freeze of os, lets try this fix

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo apt install wget -y
 wget -O zulu-jdk.deb 'https://cdn.azul.com/zulu/bin/zulu23.30.13-ca-jdk23.0.1-linux_arm64.deb?_gl=1*9354sk*_gcl_au*MTQxMTg2Mzg5Mi4xNzM2Mjc3Nzg0*_ga*NjM5NTk4NDU5LjE3MzYyNzc3ODQ.*_ga_42DEGWGYD5*MTczNjI3Nzc4NC4xLjEuMTczNjI3ODEyMC41OS4wLjA.'
 sudo dpkg -i zulu-jdk.deb
 sudo apt --fix-broken install -y
-rm zulu-jdk.deb
+rm zulu-jdk.deb  # Remove .deb to save space
 
 # Create server directory and download PaperMC
 mkdir -p papermcserver
@@ -40,6 +40,12 @@ echo -e "\e[32mServer stopped.\e[0m"
 echo -e "\e[32mAccepting EULA...\e[0m"
 echo "eula=true" > eula.txt
 
+# Enable RCON in server.properties
+echo -e "\e[32mEnabling RCON for stopping the server...\e[0m"
+echo "enable-rcon=true" >> server.properties
+echo "rcon.port=25575" >> server.properties
+echo "rcon.password=myStrongPassword123!" >> server.properties
+
 # Start the server to load plugins
 echo -e "\e[32mStarting the server to load plugins...\e[0m"
 java -Xms1G -Xmx2G -jar papermcserver.jar nogui &
@@ -51,11 +57,10 @@ while [ ! -f "plugins/Geyser-Spigot/config.yml" ]; do
   sleep 1
 done
 
-# Stop the server gracefully after plugins are initialized
+# Send `/stop` to the server via RCON to stop it gracefully
 echo -e "\e[32mStopping the server gracefully after plugins are initialized...\e[0m"
-kill -SIGTERM "$PLUGIN_SERVER_PID"
-wait "$PLUGIN_SERVER_PID" 2>/dev/null  # Wait for the process to exit completely
-echo -e "\e[32mServer stopped.\e[0m"
+mcrcon -H 127.0.0.1 -P 25575 -p myStrongPassword123! "stop"
+sleep 5  # Give the server time to shut down
 
 # Modify GeyserMC auth-type to floodgate
 echo -e "\e[32mModifying GeyserMC config.yml...\e[0m"
